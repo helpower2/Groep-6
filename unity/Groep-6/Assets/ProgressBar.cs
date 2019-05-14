048e4bb822f2a12bcba8b2a309710cc90fd694c7
helpower2 <jeffreymonnink@gmail.com>
2019-05-14 21:35:50 +0200

    added a Progress bar

---
+++ unity/Groep-6/Assets/ProgressBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+public class ProgressBar : MonoBehaviour
+{
+    RectTransform barback, bar;
+    public void SetFloat(float value)
+    {
+        bar.localScale = new Vector3(Mathf.Lerp(0, barback.localScale.y, value / 100), barback.localScale.y, barback.localScale.z);
+    }
+    private void Start()
+    {
+        barback = GetComponent<RectTransform>();
+        bar = this.transform.GetChild(0).GetComponent<RectTransform>();
+    }
+}
