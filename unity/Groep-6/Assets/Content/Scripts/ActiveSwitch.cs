4b3cd5620e95bc78a3ed1e4f28c0f7193ace0c47
helpower2 <jeffreymonnink@gmail.com>
2019-05-14 11:06:44 +0200

    human Mover!

---
+++ unity/Groep-6/Assets/Content/Scripts/ActiveSwitch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveSwitch : MonoBehaviour
+{
+    public void Switch(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
---
+++ unity/Groep-6/Assets/HumanControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanControls : MonoBehaviour
+{
+    public GameObject human;
+    public void SetTarget(GameObject Target)
+    {
+        if (human != null)
+        {
+            human.GetComponent<HumanMover>().SetTarget(Target);
+            human = null;
+        }
+    }
+    public void SetHuman(GameObject _human)
+    {
+        human = _human;
+    }
+}
