6e4816a66219e17643b8f0866fe661fcc2ce0636
Mobjunk <34821598+Mobjunk@users.noreply.github.com>
2019-05-15 11:18:14 +0200

    Money backroom

---
+++ unity/Groep-6/Assets/Content/Scripts/Money/Money.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public static Money instance;
+
+    public Action<float> onValueChange = delegate { };
+
+    [SerializeField]
+    //The amount of money the player has
+    float totalMoney;
+
+    public float TotalMoney
+    {
+        get { return totalMoney; }
+        set { totalMoney = value; }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
