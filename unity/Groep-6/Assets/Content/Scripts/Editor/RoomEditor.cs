006685fbe2dd37344d52b04dc5008e92185f320f
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2019-05-15 10:00:39 +0200

    RoomHandler prefab

---
+++ unity/Groep-6/Assets/Content/Scripts/Editor/RoomEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(BaseRoomData))]
+public class RoomEditor : Editor
+{
+    //public override void OnInspectorGUI()
+    //{
+    //    if (GUILayout.Button("UpdateRoom"))
+    //    {
+            
+    //    }
+    //}
+}
