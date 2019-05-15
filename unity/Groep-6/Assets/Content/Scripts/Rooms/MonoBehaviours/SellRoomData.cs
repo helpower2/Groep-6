e782cf2eabe68939ee6d36348dd5efc4f1653974
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2019-05-15 12:01:35 +0200

    Sell room, finally working

---
+++ unity/Groep-6/Assets/Content/Scripts/Rooms/MonoBehaviours/SellRoomData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+[System.Serializable, RequireComponent(typeof(BaseRoomData))]
+public class SellRoomData : MonoBehaviour
+{
+    [HideInInspector]
+    public BaseRoomData roomData;
+    public void Start()
+    {
+        roomData = gameObject.GetComponent<BaseRoomData>();
+        roomData.Initialize(BaseRoomData.ContentSprite.sellRoom);
+    }
+}
