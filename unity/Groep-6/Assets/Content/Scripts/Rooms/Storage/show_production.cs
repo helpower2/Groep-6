7625b77fed0181ac61f39fe55401c112a33736e3
teunjr <teunjr@hotmail.com>
2019-05-20 11:25:16 +0200

    s=money and product not 100

---
+++ unity/Groep-6/Assets/Content/Scripts/Rooms/Storage/show_production.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class show_production : MonoBehaviour
+{
+
+
+    private Text ProductDisplay;
+    int TotalUse;
+
+    void Start()
+    {
+        TotalUse = 3;
+       ProductDisplay = GetComponent<Text>();
+        
+    }
+
+
+    void Update()
+    {
+        ProductDisplay.text = "your productions = " + TotalUse.ToString();
+    }
+
+
+}
---
+++ unity/Groep-6/Assets/Content/Scripts/UI/totalmoney.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowMoney : MonoBehaviour
+{
+
+
+    private Text moneyDisplay;
+    
+
+    void Start()
+    {
+
+        moneyDisplay = GetComponent<Text>();
+
+        //als money woord geroept dan veranderen al code die aan dit vast zit
+        
+        Money.instance.onValueChange += (float money) => { MoneyCount(); };
+    }
+
+
+    void MoneyCount()
+    {
+        //in money display word je geld uit gespewd
+        moneyDisplay.text = "your money = " + Money.instance.TotalMoney.ToString();
+    }
+
+
+}
